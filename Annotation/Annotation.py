a1bd78b8982f5528ce9502d1814410d9bca84738
Glas42 <145870870+Glas42@users.noreply.github.com>
2024-08-18 20:38:20 +0200

    added an icon and added some more code to the annotation script

--- Annotation/Annotation.py
+++ Annotation/Annotation.py
@@ -1,4 +1,6 @@
 import numpy as np
+from ctypes import windll, byref, sizeof, c_int
+import win32gui, win32con
 import cv2
 import os
 
@@ -9,9 +11,31 @@ print("Caching images...")
 
 images = []
 for file in os.listdir(DATA_PATH):
-    if (file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg")) and os.path.exists(f"{DATA_PATH}\\{file.replace(file.split('.')[-1], 'txt')}") == False:
+    if (file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg")) and os.path.exists(f"{PATH}\\Datasets\\FinalDataset\\{file.replace(file.split('.')[-1], 'txt')}") == False:
         image = cv2.imread(f"{DATA_PATH}\\{file}", cv2.IMREAD_UNCHANGED)
         images.append(image)
 
 print("Done!")
 
+def CreateWindow():
+    cv2.namedWindow("LaneDetection - Annotation", cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty("LaneDetection - Annotation", cv2.WND_PROP_TOPMOST, 1)
+    if os.name == 'nt':
+        hwnd = win32gui.FindWindow(None, "LaneDetection - Annotation")
+        windll.dwmapi.DwmSetWindowAttribute(hwnd, 35, byref(c_int(0x000000)), sizeof(c_int))
+        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+        hicon = win32gui.LoadImage(None, f"{PATH}\\icon.ico", win32con.IMAGE_ICON, 0, 0, icon_flags)
+        win32gui.SendMessage(hwnd, win32con.WM_SETICON, win32con.ICON_SMALL, hicon)
+        win32gui.SendMessage(hwnd, win32con.WM_SETICON, win32con.ICON_BIG, hicon)
+
+index = 0
+
+while index < len(images):
+    try:
+        _, _, _, _ = cv2.getWindowImageRect("LaneDetection - Annotation")
+    except:
+        CreateWindow()
+
+    cv2.imshow("LaneDetection - Annotation", images[index])
+    cv2.waitKey(0)
+    index += 1
